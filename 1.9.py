ff195a3ac76e1660dca14c753e7854d967f8e593
Ruslan <ruslanmorozov357@gmail.com>
2022-05-17 21:14:47 +0300

    second

--- 1.9.py
+++ 1.9.py
@@ -1,6 +1,6 @@
-#  Глава 1 Упражнение 1.3 (страница 28)
+#Exercise 2.3
 
 distance_km=10
-time=43.5/60
+time=42.42/60
 distance_mile = distance_km/1.61
 print('Средняя скорость = ', distance_mile/time, ' миль/час')
