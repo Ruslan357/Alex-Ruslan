d1ae54518d3a210e3ac44328a5dd63f1d5d8643c
Ruslan <ruslanmorozov357@gmail.com>
2022-05-19 19:10:06 +0300

    fixed

--- 1.9.py
+++ 1.9.py
@@ -1,6 +1,14 @@
-#Exercise 2.3
+#Exercise 2.1
+answer1 = 42*60+42
+print(f'There are {answer1} seconds in 42 minutes 42 seconds\n')
 
+#Exercise 2.2
+answer2  = 10/1.61
+print (f'There are {round(answer2,2)} miles in 10 kilometers\n')
+
+#Exercise 2.3
 distance_km=10
 time=42.42/60
 distance_mile = distance_km/1.61
-print('Средняя скорость = ', distance_mile/time, ' миль/час')
+answer3 = distance_mile/time
+print(f'The average speed is {round(answer3,2)} kilometers per hour\n')
--- 2.10.py
+++ 2.10.py
@@ -1,20 +1,26 @@
+import math   
+    #Exercise 2.1
+
+radius = 5
+volume_of_sphere = 4/3 * math.pi * radius
+print(f'The volume of the ball is equal to {round(volume_of_sphere,2)}\n')
+
     #Exercise 2.2
-price=24.95
-discount=0.4
-delivery_firs=3
-delivery_after=0.75
-number_of_books=60
+price = 24.95
+discount = 0.4
+delivery_firs = 3
+delivery_after = 0.75
+number_of_books = 60
 
-total = number_of_books*((price*discount))-(delivery_after*(number_of_books-1)-delivery_firs)
+total = number_of_books * ((price - (price*discount))) - (delivery_after * (number_of_books - 1) - delivery_firs)
 
-print('Цена покупки 60 книг с доставкой = ', round(total,3), '$')
-
+print(f'The purchase price of 60 books with delivery is = {round(total,3)}$\n')
 
     #Exercise 2.3
-start=(6*60+52)/60
-round1=(8*60+15)/60
-round2=3*((7*60+12)/60)
-round3=round1
-time_of_run=round1+round2+round3
+start = (6 * 60 + 52) / 60
+round1 = (8 * 60 + 15) / 60
+round2 = 3 * ((7 * 60 + 12)) / 60
+round3 = round1
+time_of_run = round1 + round2 + round3
 
-print('Я вернусь домой через',time_of_run,'минут')
+print(f'I will return home in {time_of_run} minutes\n')
