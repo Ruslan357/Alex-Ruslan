0a20f86eebf882418834a1aaf52f8d26a4ca4852
Ruslan <ruslanmorozov357@gmail.com>
2022-05-20 20:28:59 +0300

    fixed

--- 1.9.py
+++ 1.9.py
@@ -4,11 +4,11 @@ print(f'There are {answer1} seconds in 42 minutes 42 seconds\n')
 
 #Exercise 2.2
 answer2  = 10/1.61
-print (f'There are {round(answer2,2)} miles in 10 kilometers\n')
+print (f'There are {answer2} miles in 10 kilometers\n')
 
 #Exercise 2.3
 distance_km=10
 time=42.42/60
 distance_mile = distance_km/1.61
 answer3 = distance_mile/time
-print(f'The average speed is {round(answer3,2)} kilometers per hour\n')
+print(f'The average speed is {answer3} kilometers per hour\n')
