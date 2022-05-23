7f5ad29358d243e3ba65c77c17d961304f23d3e8
Ruslan <ruslanmorozov357@gmail.com>
2022-05-23 21:18:57 +0300

    fixed

--- 1.9.py
+++ 1.9.py
@@ -8,7 +8,12 @@ print (f'There are {answer2} miles in 10 kilometers\n')
 
 #Exercise 2.3
 distance_km=10
-time=42.42/60
+time=42.42
 distance_mile = distance_km/1.61
-answer3 = distance_mile/time
-print(f'The average speed is {answer3} kilometers per hour\n')
+average_pace_per_min = distance_mile/time
+average_pace_per_sec = average_pace_per_min/60
+average_speed_per_hour = average_pace_per_min*60
+
+print(f'The average pace {average_pace_per_min} per mile/minutes\n')
+print(f'The average pace {average_pace_per_sec} per mile/seconds\n')
+print(f'The average speed is {average_speed_per_hour} miles/hour\n')
--- 2.10.py
+++ 2.10.py
@@ -2,7 +2,7 @@ import math
     #Exercise 2.1
 
 radius = 5
-volume_of_sphere = 4/3 * math.pi * radius
+volume_of_sphere = 4/3 * math.pi * radius**3
 print(f'The volume of the ball is equal to {volume_of_sphere}\n')
 
     #Exercise 2.2
@@ -12,15 +12,18 @@ delivery_firs = 3
 delivery_after = 0.75
 number_of_books = 60
 
-total = (number_of_books * (price - (price*discount))) - (delivery_after * (number_of_books - 1)) - delivery_firs
+total = (number_of_books * (price - (price*discount))) + (delivery_after * (number_of_books - 1)) + delivery_firs
 
 print(f'The purchase price of 60 books with delivery is = {total} $\n')
 
     #Exercise 2.3
-start = (6 * 60 + 52) / 60
-round1 = (8 * 60 + 15) / 60
-round2 = 3 * ((7 * 60 + 12)) / 60
-round3 = round1
-time_of_run = round1 + round2 + round3
+              
+slow_pace = 8*60+15 
+fast_pace = 7*60+12
+start = 24720                                     #6:52 is 24,720 seconds from the start of the day
+total = start + 2* slow_pace + 3 * fast_pace       
+hours = math.floor(total/3600)
+minutes = math.floor((total - (hours * 3600))/60)
+seconds = (total - (hours * 3600)) - (minutes*60)
 
-print(f'I will return home in {time_of_run} minutes\n')
+print(f'I will return home at {hours:02}:{minutes:02}:{seconds:02}\n')
