d2ac7c0674d3aa1bb9f969c1704d59684067e295
Ruslan <ruslanmorozov357@gmail.com>
2022-05-27 01:05:10 +0300

    fix with Deciamal 26.05.22

--- 1.9.py
+++ 1.9.py
@@ -10,10 +10,10 @@ print (f'There are {answer2} miles in 10 kilometers\n')
 distance_km=10
 time=42.42
 distance_mile = distance_km/1.61
-average_pace_per_min = distance_mile/time
-average_pace_per_sec = average_pace_per_min/60
-average_speed_per_hour = average_pace_per_min*60
+average_pace_min = time/distance_mile
+average_pace_sec = average_pace_min*60
+average_speed_per_hour = distance_mile/time*60
 
-print(f'The average pace {average_pace_per_min} per mile/minutes\n')
-print(f'The average pace {average_pace_per_sec} per mile/seconds\n')
+print(f'The average pace {average_pace_min} min/mile\n')
+print(f'The average pace {average_pace_sec} sec/mile\n')
 print(f'The average speed is {average_speed_per_hour} miles/hour\n')
--- 2.10.py
+++ 2.10.py
@@ -1,4 +1,5 @@
-import math   
+import math
+from decimal import Decimal   
     #Exercise 2.1
 
 radius = 5
@@ -12,9 +13,9 @@ delivery_firs = 3
 delivery_after = 0.75
 number_of_books = 60
 
-total = (number_of_books * (price - (price*discount))) + (delivery_after * (number_of_books - 1)) + delivery_firs
-
-print(f'The purchase price of 60 books with delivery is = {total} $\n')
+total = (number_of_books * (price - (price * discount))) + (delivery_after * (number_of_books - 1)) + delivery_firs
+answer = Decimal(str(total))
+print(f'The purchase price of 60 books with delivery is = {answer.quantize(Decimal("1.00"))} $\n')
 
     #Exercise 2.3
               
@@ -22,8 +23,8 @@ slow_pace = 8*60+15
 fast_pace = 7*60+12
 start = 24720                                     #6:52 is 24,720 seconds from the start of the day
 total = start + 2* slow_pace + 3 * fast_pace       
-hours = math.floor(total/3600)
-minutes = math.floor((total - (hours * 3600))/60)
+hours = total//3600
+minutes = (total - (hours * 3600))//60
 seconds = (total - (hours * 3600)) - (minutes*60)
 
 print(f'I will return home at {hours:02}:{minutes:02}:{seconds:02}\n')
