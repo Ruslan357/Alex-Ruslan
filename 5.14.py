7eb4c26a070ecb06be06c59f4a95422f318c85be
Ruslan <ruslanmorozov357@gmail.com>
2022-06-14 00:33:50 +0300

    add exercises to 5.14.py 13.06.2022

--- 5.14.py
+++ 5.14.py
@@ -1,3 +1,16 @@
+        # Exercise 1
+import time
+start = int(time.time())
+
+hours = start//3600
+minutes = (start - (hours * 3600))//60
+seconds = (start - (hours * 3600)) - (minutes*60)
+days = start//3600//24
+hours_in_time = int(((start//3600/24)-(start//3600//24))*24)
+
+print(f'\nDays have passed: {days}')
+print(f'Time: {hours_in_time:02}:{minutes:02}:{seconds:02} (UTC)\n')
+        
         # Exercise 2
 #2.1
 def check_fermat(a,b,c,n):
@@ -17,7 +30,6 @@ def conversion():
 conversion()
 
         # Exercise 3
-
 #3.1
 def is_triangle(segment_1, segment_2, segment_3):
     if segment_1>segment_2+segment_3 or segment_2>segment_1+segment_3 or segment_3>segment_1+segment_2:
@@ -35,3 +47,19 @@ def conversion():
 conversion()
 
 is_triangle(12,1,1)
+
+        # Exercise 4
+
+# 4.1  Answer: the program will give an error due to exceeding the allowed number of recursions (infinite recursion)
+
+# 4.2
+def recurse(n, s):
+    """ n,s - variables that change their values during a recursive function call. 
+        The function outputs the value of c when the value of n reaches 0
+    """
+    if n == 0:
+        print(s)
+    else:
+        recurse(n-1, n+s)
+
+recurse(3, 0)
